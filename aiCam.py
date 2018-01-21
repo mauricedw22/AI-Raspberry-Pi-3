338482d1ca9e8e1fa5e40e4a0fdc9421eb5bf956
Bryan Clark <bclark2@umbc.edu>
2018-01-20 22:36:16 -0800

    changed variable name added comments.

--- aiCam.py
+++ aiCam.py
@@ -27,17 +27,21 @@ if pir.wait_for_motion() == True:
    camera.close()
    print("Movement")
 
-   #IBM Watson post
+   # IBM Watson post
    url   = 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key='YOUR-WATSON-API-KEY-HERE'&version=2016-05-20'
    files = {'images_file': open('/home/pi/Desktop/motionCam/images/image.png','rb')}
    req   = requests.post(url, files=files)
 
    data = req.json()
 
-   #workaround for Python returning the JSON object as a dictionary that is very hard to work with.
-   str2 = 'person'
+   # workaround for Python returning the JSON object as a dictionary that is very hard to work with.
+   # find() function returns -1 if the string is not matched. Index of string if it is matched.
+   # the string to find variable is the classification that IBM Watson gives to a person or a group of people.
+   # If you want to look for something other than a person, change the stringToFind variable, but check out the -
+   # IBM Watson response object to see the types of classification strings that are used, so that you know what to look for. 
+   stringToFind = 'person'
 
-   if str(data).find(str2) > -1:
+   if str(data).find(stringToFind) > -1:
       message = client.messages.create(
         to    = "PHONE-YOU-WANT-TO-REACH",
         from_ = "YOUR-TWILIO-NUMBER",
